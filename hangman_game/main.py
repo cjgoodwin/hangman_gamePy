cc5fbbfb0f61c11a9b51d2e4d47e72362335690a
cjgoodwin <c.jgoodwin@yahoo.com>
2021-08-28 19:14:43 -0500

    initial commit

---
+++ hangman_game/main.py
@@ -0,0 +1,16 @@
+# First we need to set up some kind of array to keep track of the word and the hidden list
+
+# My objectives to include as the programmer
+    # keep looping until the player has won or lost
+
+    # Display the guessed letters, the current boards, and how many attempts are remaining
+
+    # ask for user input (the letter they want to guess)
+
+        # if the user's guess is correct, show all the matched letters and print the message
+
+        # if the user's guess is wrong, print failure message and how many attempts are left
+
+    # If user wins the game, print a win message and stop the loop from executing
+
+    # If user looses, print failing message and stop the loop from executing
